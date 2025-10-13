da8cee128889375a089c73144550ea6c47c09366
sparkle <wkyuu@majo.im>
2025-10-13 09:17:45 +0800

    u1x

--- app/18-mysql/init.sql
+++ app/18-mysql/init.sql
@@ -1,14 +1,14 @@
 DELETE FROM mysql.user WHERE user = 'root' AND host = '%';
 
-CREATE USER 'phpmyadmin'@'localhost' IDENTIFIED BY 'phpmyadmin';
+CREATE USER 'phpmyadmin'@'localhost' IDENTIFIED BY 'PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
 
 CREATE DATABASE typecho;
-CREATE USER 'typecho'@'localhost' IDENTIFIED BY 'typecho';
+CREATE USER 'typecho'@'localhost' IDENTIFIED BY 'PASSWORD';
 GRANT ALL PRIVILEGES ON typecho.* TO 'typecho'@'localhost';
 
 CREATE DATABASE hedgedoc;
-CREATE USER 'hedgedoc'@'%' IDENTIFIED BY 'hedgedoc';
+CREATE USER 'hedgedoc'@'%' IDENTIFIED BY 'PASSWORD';
 GRANT ALL PRIVILEGES ON hedgedoc.* TO 'hedgedoc'@'%';
 
 FLUSH PRIVILEGES; 
