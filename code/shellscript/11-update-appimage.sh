478c6f92c939f2a202c1b3b9c946d19f2046bde7
sparkle <wkyuu@majo.im>
2025-10-15 15:40:39 +0800

    u1x:wq

---
+++ code/shellscript/11-update-appimage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+rss_url="https://cursor.com/api/download?platform=linux-x64&releaseTrack=stable"
+download_url=$(curl -fsL $rss_url | jq -r '.downloadUrl')
+version=$(echo $download_url | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}")
+
+appname="cursor"
+platform="linux"
+arch="x86_64"
+appimage_name="${appname}-${platform}-${arch}-${version}.AppImage"
+desktop_file_name="${appname}.desktop"
+
+if [ ! -f $appimage_name ]; then
+	echo "downloading ${appname} via $download_url"
+	wget -vO $appimage_name $download_url
+	if [ $? -ne 0 ]; then
+		echo "failed to download $download_url, try \nwget -vN $download_url -O $appimage_name"
+		exit 1
+	fi
+fi
+
+chmod +x $appimage_name
+./$appimage_name --appimage-extract
+rsync -avtz --progress squashfs-root/* .
+
+if [ $(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}' $desktop_file_name) == $version ]; then
+	rm -rf squashfs-root
+	rm $appimage_name
+fi
+
+echo -e "\n\n> ${appname} updated to $version done."
