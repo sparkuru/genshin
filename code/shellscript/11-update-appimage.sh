cc092afbdda35fb56d79e5725a9fa3b44d4f83e7
sparkle <wkyuu@majo.im>
2025-10-29 18:04:11 +0800

    u1x:wq

--- code/shellscript/11-update-appimage.sh
+++ code/shellscript/11-update-appimage.sh
@@ -12,7 +12,13 @@ arch="x86_64"
 appimage_name="${appname}-${platform}-${arch}-${version}.AppImage"
 desktop_file_name="${appname}.desktop"
 
-if [ ! -f $appimage_name ]; then
+current_version=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}' $desktop_file_name)
+if [ $current_version == $version ]; then
+	echo "already up to date, current version: $current_version, latest version: $version"
+	exit 0
+fi
+
+if [[ ! -f $appimage_name || ! -s $appimage_name ]]; then
 	echo "downloading ${appname} via $download_url"
 	wget -vO $appimage_name $download_url
 	if [ $? -ne 0 ]; then
