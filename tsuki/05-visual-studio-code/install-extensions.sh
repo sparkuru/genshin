60bf2fdc32bcefc3e7c756f1b2c17a369556eb82
sparkle <wkyuu@majo.im>
2025-12-01 09:32:30 +0800

    u1x:wq

---
+++ tsuki/05-visual-studio-code/install-extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# code --list-extensions | xargs -L 1 code --install-extension
+
+
+github_content_base="https://raw.githubusercontent.com/sparkuru/genshin/main"
+extension_url="$github_content_base/tsuki/05-visual-studio-code/extension.txt"
+extensions_file="/tmp/extension.txt"
+
+curl -sfL -o $extensions_file $extension_url
+cat $extensions_file | xargs -L 1 code --install-extension
