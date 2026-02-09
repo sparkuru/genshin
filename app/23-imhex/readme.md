7e3629937838ff3bdae6c0d6f8adab1ab19d4a04
sparkuru <wkyuu@majo.im>
2026-02-09 18:09:19 +0800

    u1x:wq

---
+++ app/23-imhex/readme.md
@@ -0,0 +1,3 @@
+
+1. `git clone https://github.com/WerWolv/ImHex.git repo`
+2. 
--- code/shellscript/15-plain-file-convertor.sh
+++ code/shellscript/15-plain-file-convertor.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-files=$(find . \( -name "*.c" -o -name "*.h" \) -type f | \
-        xargs enca -L zh | grep GB2312 | cut -d: -f1)
 
-for f in $files; do
-  iconv -f GB2312 -t UTF-8 "$f" | dos2unix > "$f.tmp" && mv "$f.tmp" "$f"
-done
+find . -type f -print0 \
+  | xargs -0 -r file --mime-type \
+  | awk -F: '$2 ~ /text\// {print $1}' \
+  | xargs -r -d '\n' enca -L zh \
+  | awk -F: '$2 !~ /utf-8/i && $2 ~ /(GB2312|GBK|GB18030)/ {print $1}' \
+  | while IFS= read -r f; do
+      iconv -f GB18030 -t UTF-8 -c "$f" | dos2unix > "$f.tmp" && mv "$f.tmp" "$f"
+    done
